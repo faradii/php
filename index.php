65fcd5138c82b17e0250bc9dc6494b8ffac28b31
Azmarei <faazmarei@itab.bfwit-ab.de>
2024-10-14 10:57:19 +0200

    erster commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+
+<html>
+<head>
+</head>
+<body> 
+
+
+<h1>
+Mein Titel der HauptSeite
+</h1>
+<a href="/dashboard.html">hier klicken</a>
+</body>
+ </html>
+
+<?php
+
+echo "das ist php code mit echo";
+?>
